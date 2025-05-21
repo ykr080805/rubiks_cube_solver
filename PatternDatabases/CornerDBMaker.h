a67157d032695a306cb0fa1ad94d3af98ae009e7
ykr <r.yeredla@iitg.ac.in>
2025-05-21 19:39:51 +0530

    added CornerDBMaker.h

---
+++ PatternDatabases/CornerDBMaker.h
@@ -0,0 +1,25 @@
+//
+// Created by koush on 21-05-2025.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
+#define RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
+#include "CornerPatternDatabase.h"
+#include "../Model/RubiksCubeBitboard.cpp"
+
+using namespace std;
+
+class CornerDBMaker {
+private:
+    string fileName;
+    CornerPatternDatabase cornerDB;
+
+public:
+    CornerDBMaker(string _fileName);
+    CornerDBMaker(string _fileName, uint8_t init_val);
+
+    bool bfsAndStore();
+};
+
+
+#endif //RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
